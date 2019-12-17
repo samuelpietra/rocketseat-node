434bca27d1fdf0b787b4220920bd746cecd22067
samuel <samuelpietra@outlook.com>
2019-12-17 09:12:46 -0300

    API NODE concluída

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+// Iniciando o app
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+// Iniciando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true }
+);
+requireDir('./src/models');
+
+// Rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
